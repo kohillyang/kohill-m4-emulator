c44e9219f2cde5c86eabccf2dbc2955664de2858
Unknown <kohillyang@126.com>
2017-06-14 23:29:00 +0800

    implement lsl with carry support

---
+++ inc/m4_core.h
@@ -0,0 +1,19 @@
+/*
+ * m4_core.h
+ *
+ *  Created on: 2017Äê6ÔÂ14ÈÕ
+ *      Author: kohil
+ */
+
+#ifndef INC_M4_CORE_H_
+#define INC_M4_CORE_H_
+
+#define CPSR_BIT_N (1<<31)
+#define CPSR_BIT_Z (1<<30)
+#define CPSR_BIT_C (1<<29)
+#define CPSR_BIT_V (1<<28)
+
+
+
+
+#endif /* INC_M4_CORE_H_ */
--- inc/m4_core.hpp
+++ inc/m4_core.hpp
@@ -6,6 +6,7 @@
  */
 #include "public_header.hpp"
 #include "IntelHexAdapter.hpp"
+#include "m4_core.h"
 namespace M4_core_thumb16 {
 class CORE_REGS : Object{
 private:
@@ -18,13 +19,13 @@ public:
 			rstr = rstr +QString("r%1:").arg(i,0,10)
 					+ Int2Hex(r[i]) + " "
 					;
-//			cout <<r[i] <<endl;
 		}
+	rstr += QString("cpsr:") + Int2Hex(cpsr);
 		return rstr;
 	}
 	CORE_REGS(){
 		memset(r, 0, sizeof(r));
-		cpsr = 0;
+		cpsr = 0x173;
 	}
 	uint32_t& operator [](uint32_t index) {
 		AbortAssert(index < 16, QString("index out of bound.%0").arg(index));
@@ -35,11 +36,34 @@ public:
 		return r[index];
 	}
 	void updateCpsrByResult(uint32_t r) {
-		cpsr |= (0b1 << 31) & r; //N
-		r ? (cpsr |= (1 << 30) & r) : (cpsr &= ~(1 << 30) & r); //Z
-		//C And N flags are only for Add and subtract operations.
+		cpsr |= CPSR_BIT_N & r; //N
+		r ? (cpsr &= ~CPSR_BIT_Z):(cpsr |= CPSR_BIT_Z) ; //Z
+//		Copy N bit to CPSR from r.
+		cpsr &= ~CPSR_BIT_N;
+		cpsr |= r & CPSR_BIT_N; //N
+
+	}
+	void updateCpsrCarryBit(bool C){
+		C ? (cpsr |= CPSR_BIT_C) : (cpsr &= ~CPSR_BIT_C);
 	}
 };
+static uint32_t mask_table(uint32_t i){
+	return (1<<i) -1;
+}
+//static uint32_t BITS(uint32_t d,uint32_t startbit,uint32_t stopbit){
+//	AbortAssert(startbit <=31,"");
+//	AbortAssert(stopbit <=31,"");
+//	return (d & (mask_table(stopbit-startbit+1) << startbit )) >> startbit;
+//}
+static uint32_t BITS(uint16_t d,uint32_t startbit,uint32_t stopbit){
+	AbortAssert(startbit <=16,"");
+	AbortAssert(stopbit <=16,"");
+	return (d & (mask_table(stopbit-startbit+1) << startbit )) >> startbit;
+}
+#define LSL_I_CON(x) (BITS(x, 11, 15 ) == 0x00)
+#define LSL_R_CON(x) (BITS(x, 6, 15)  ==0x102)
+
+
 /*
  * * lsr(immediate)
  * 	lsl r0,r1,#2 -> 0x0088 -> 0b0 00010 001 000
@@ -53,26 +77,30 @@ public:
  * 	 It can optionally update the condition flags based on the result.
  *
  */
-uint32_t getBits(uint32_t d,uint32_t mask,uint32_t startbit){
-	return (d & (mask << startbit )) >> startbit;
-}
 uint32_t lsl(CORE_REGS &core_regs, uint16_t instruction) {
-	if((instruction & (0b11111 << 11)) == 0){
+	if(LSL_I_CON(instruction)){
 		uint16_t imm5 = (instruction & (0b11111 << 6)) >> 6;
 		uint32_t rm = (instruction & (0b111 << 3)) >> 3;
 		uint32_t rd = (instruction & (0b111 << 0)) >> 0;
-		uint32_t result = core_regs[rm] << imm5;
+		uint32_t fromData = core_regs[rm];
+		uint32_t result = fromData << imm5;
 		core_regs[rd] = result;
 		core_regs.updateCpsrByResult(result);
+		((1 << (32 - imm5)) & fromData) ? core_regs.updateCpsrCarryBit(true) :
+				core_regs.updateCpsrCarryBit(false);
 		return result;
 	}//lsr(immediate)
-	else if(getBits(instruction, 0b1111111111, 6) == 0b0100000010){
+	else if(LSL_R_CON(instruction)){
 		uint32_t rm = (instruction & (0b111 << 3)) >> 3;
 		uint32_t rd = (instruction & (0b111 << 0)) >> 0;
 		uint32_t n = core_regs[rm] & 0xff;
-		uint32_t result = core_regs[rm] << n;
+		uint32_t fromData = core_regs[rm];
+		uint32_t result = fromData << n;
 		core_regs[rd] = result;
 		core_regs.updateCpsrByResult(result);
+		((1 << (32 - n)) & fromData) ? core_regs.updateCpsrCarryBit(true) :
+					core_regs.updateCpsrCarryBit(false);
+
 		return result;
 	}
 	else{
@@ -94,9 +122,15 @@ private:
 	void unitTest() {
 		//for lsr lsl r0,r1,#2
 		core_regs[0] = 2;
-		core_regs[1] = 3;
-		lsl(core_regs, 0x0088);
-		std::cout << "lsr lsl r0,r1,#2 " << (core_regs[0] == 0x0c ? "pass":"not pass") << std::endl;
+		core_regs[1] = 5;
+		lsl(core_regs, 0x788);//lsl r0,r1,#30
+		cout << core_regs.toString() << endl;
+		//expect r0:0x40000000 r1:0x5 r2:0x0 cpsr:0x20000173
+		core_regs[0] = 2;
+		core_regs[1] = 5;
+		lsl(core_regs, 0x7c8);//lsl r0,r1,#31
+		cout << core_regs.toString() << endl;
+		//expect r0:0x80000000 r1:0x5 r2:0x0 cpsr:0x80000173
 	}
 };
 
